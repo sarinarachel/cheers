fb3b99834fcf6aa491df38f590732c083bbc6b24
sarinarachel <skatz13@uw.edu>
2020-02-19 19:09:20 -0800

    Start grommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useContext, useState} from 'react';
 import './App.css';
+import { Box, Button, Grommet, TextInput } from 'grommet';
+import { Search } from "grommet-icons";
+import { MDBIcon } from 'mdbreact';
+
+const context = React.createContext()
+
+const customTheme = {
+  global: {
+    font: {
+      family: "Roboto"
+    }
+  },
+  button: {
+    border: {
+      radius: "4px"
+    },
+    padding: {
+      vertical: "4px",
+      horizontal: "4px"
+    },
+  }
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setState] = useState({
+    searchTerm:''
+  })
+  return <context.Provider value={{
+    ...state, 
+    set: v=> setState(current=> {
+      return{...current, ...v}})
+  }}>
+    <Grommet theme={customTheme}>
+      <Header />
+    </Grommet>
+  </context.Provider>
 }
+
+function Header(){
+  const ctx = useContext(context)
+  const {searchTerm} = ctx
+  return <Box tag='header' direction='row' align='center' justify='between' pad={{ left: 'medium', right: 'medium', vertical: 'medium' }}>
+    <Button 
+      className="cocktail"
+      plain={false} 
+      onClick={() => {}} 
+    />
+    <Button 
+      className="ingredient"
+      plain={false} 
+      style={{ marginLeft: '8px', marginRight: '12px' }}
+      onClick={() => {}} 
+    >
+      <MDBIcon icon="cocktail" />
+    </Button>
+    <TextInput
+      className="input"
+      placeholder="Search Cocktails"
+      value={searchTerm}
+      onChange={e=> ctx.set({searchTerm: e.target.value})}
+    />
+    <Button 
+      className="search"
+      plain={false} 
+      icon={<Search />} 
+      primary
+      color='#FFCA58'
+      style={{ marginLeft: '8px' }}
+      onClick={() => {}} 
+    />
+  </Box>
+  
+}
+
+
 
 export default App;
