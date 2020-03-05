95bddf14188d9b7e275be6d2e3bd25263e1cbf03
sarinarachel <skatz13@uw.edu>
2020-03-04 18:59:34 -0800

    Ingredient Search and random added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, {useContext, useState} from 'react';
 import './App.css';
-import { Box, Button, Grommet, TextInput, ThemeContext } from 'grommet';
-import { Image, Bar, Cafeteria, Filter, Search, Update } from "grommet-icons";
-import { MDBIcon } from 'mdbreact';
+import { Box, Button, Grommet, Heading, Paragraph, ResponsiveContext, Text, TextInput, ThemeContext } from 'grommet';
+import { Bar, Cafeteria, Filter, Search, Update } from "grommet-icons";
+// import { MDBIcon } from 'mdbreact';
 
 const context = React.createContext()
 
 const customTheme = {
-  global: {
-    font: {
-      family: "Roboto"
-    }
-  },
   button: {
     border: {
       radius: "4px"
@@ -25,7 +20,7 @@ const customTheme = {
 
 function App() {
   const [state, setState] = useState({
-    searchTerm:''
+    searchTerm:'', mode:'cocktail'
   })
   return <context.Provider value={{
     ...state, 
@@ -42,30 +37,33 @@ function App() {
 
 function Header(){
   const ctx = useContext(context)
-  const {loading, searchTerm} = ctx
-  return <Box> 
+  const {loading, searchTerm, mode} = ctx
+  const screenSize = React.useContext(ResponsiveContext);
+  return <Box>
     <Box tag='header' direction='row' align='center' justify='between' pad='medium'>
       <Button 
         className="cocktail"
         icon={<Bar />}
         plain={false} 
-        onClick={() => {}} 
+        color={mode==='cocktail' ? "#6FFFB0" : "brand"}
+        onClick={() => ctx.set({mode:'cocktail'})} 
       >
       </Button>
-
       <Button 
         className="ingredient"
         plain={false} 
         icon={<Cafeteria />}
-        style={{ marginLeft: '8px', marginRight: '12px' }}
-        onClick={()=> {}}
+        style={{ marginLeft: '4px', marginRight: '10px' }}
+        color={mode==='ingredient' ? "#6FFFB0" : "brand"}
+        onClick={()=> ctx.set({mode:'ingredient'})}
       />
 
       <TextInput
         className="input"
-        placeholder="Search Cocktails"
+        placeholder={mode==="cocktail" ? "Search Cocktails" : "Search by Ingredient"}
         value={searchTerm}
         disabled={loading}
+        style={{fontSize: screenSize==="small" ? '16px' : '20px'}}
         onChange={e=> ctx.set({searchTerm: e.target.value})}
         onKeyPress={e=>{
           if(e.key==='Enter' && searchTerm) search(ctx)
@@ -77,7 +75,7 @@ function Header(){
         icon={<Search />} 
         primary
         color='#FFCA58'
-        style={{ marginLeft: '8px' }}
+        style={{ marginLeft: '4px' }}
         onClick={() => search(ctx)} 
       />
     </Box>
@@ -89,7 +87,7 @@ function Header(){
       },
       text: { medium: { size: "16px"}}
     }}>
-      <Box pad='small' direction='row' align='start' background='#F2F2F2' justify='between'>
+      <Box pad={{horizontal:'medium', vertical:'small'}} direction='row' align='start' background='#F2F2F2' justify='between'>
         <Button
           className="random"
           primary
@@ -97,7 +95,7 @@ function Header(){
           label="Random"
           style={{ marginTop: '0px' }}
           color="#E6E1F0"
-          onClick={() => {}} 
+          onClick={() => random(ctx)} 
         />
         <Button
           className="filter"
@@ -118,48 +116,80 @@ function Body(){
   const ctx = useContext(context)
   const {error, drinks} = ctx
   if (drinks) {
-    console.log(drinks)
-    return <Box background="#E8E8E8" overflow="scroll" pad="medium"> 
+    return <Box background="#E8E8E8" overflow="scroll" pad="medium" style={{minHeight :'84.7vh'}}> 
         {drinks.map((drink,i)=> <Drink key={i} {...drink} />)}
     </Box>
-  } return <Box background="#E8E8E8" height="84.7vh" pad="medium"></Box>
+  } return <Box background="#E8E8E8" height="84.7vh" pad="medium">
+      {error && <div className="error">{error}</div>}
+    </Box>
 }
 
-async function search({searchTerm, set}){
+async function search({searchTerm, set, mode}){
   try {
     const term = searchTerm
-    set({error:'', loading:true})
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`
-    console.log(url)
+    console.log(mode)
+    set({error:'', loading:true, drinks:[]})
+    let url = ``
+    if (mode==='cocktail'){
+      url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`
+    } else {
+      url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${term}`
+    }
     const r = await fetch(url)
-    console.log(r)
     const drinkData = await r.json()
+    console.log(drinkData['drinks'])
     if(!drinkData['drinks'][0]){
       return set({error:'No drink matching that query'})
     }
     set({drinks:drinkData['drinks'], loading:false, searchTerm:''})
   } catch(e) {
-    set({error: e.message})
-  }
+    set({error: 'No drink matching that query'})
+  } set ({loading:false})
+}
+
+async function random({set}){
+  try {
+    set({error:'', loading:true})
+    const url=`https://www.thecocktaildb.com/api/json/v1/1/random.php`
+    const r = await fetch(url)
+    const drinkData = await r.json()
+    set({drinks:drinkData['drinks'], loading:false, searchTerm:''})
+  } catch(e) {
+    set({error: 'No drink matching that query'})
+  } set ({loading:false})
 }
 
 function Drink(props){
-  console.log("StrDrinkThumb " + props.strDrinkThumb)
   return (
-    <Box background="white" direction="row" height="171px" margin="small" pad="medium">
-      {/* <Box height="medium" width="large">
-        <Image
-          src="http://hdwpro.com/wp-content/uploads/2017/01/3D-Cool-Image.jpg"
-          fit="cover"
-        />
-      </Box> */}
+    <Box 
+      round="4px" 
+      elevation="medium" 
+      background="white" 
+      direction="row" 
+      height="171px" 
+      margin="small" 
+      pad="medium"
+      onClick={() => {}}>
       <img 
         alt="drink"
         src={props.strDrinkThumb}
-        style={{ height: '142px', width: '142px' }}
+        style={{ objectFit: "cover", height: '142px', width: '142px', borderRadius: '4px' }}
       />
-      <Box direction="column" margin="small">
-        {props.strDrink}
+      <Box overflow="hidden" direction="column" margin={{ "left": "medium" }}>
+        <Heading size="small" margin={{"top": "none", "bottom": "small"}}> {props.strDrink} </Heading>
+        <Box wrap={true} direction="row" margin={{"bottom":"small"}}>
+          {props.strIngredient1 && <Box margin={{"vertical":"xsmall", "right":"xsmall"}} background="#3D138D" pad={{"vertical":"xsmall", "horizontal":"medium"}} round="100px">
+            <Text size="xsmall" weight="bold"> {props.strIngredient1} </Text>
+          </Box>}
+          {props.strIngredient2 && <Box margin={{"vertical":"xsmall"}} background="#3D138D" pad={{"vertical":"xsmall", "horizontal":"medium"}} round="100px">
+            <Text size="xsmall" weight="bold"> {props.strIngredient2} </Text>
+          </Box>}
+        </Box>
+        <Box >
+        <Paragraph size="small">
+          {props.strInstructions}
+        </Paragraph>
+        </Box>
       </Box>
     </Box>
   )
